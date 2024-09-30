a4eeb85f491067d982e9e7a818cfd020e7d4cd60
Genis <genisvallespinlongares@gmail.com>
2024-09-30 16:53:16 +0200

    Funcion per calcular percentatgeGC en ADNSeq.py creada

--- ADNSeq.py
+++ ADNSeq.py
@@ -4,4 +4,14 @@ class ADNseq:
 
 
     def percentatgeGC(self):
+        counter = 0
+        for base in adn:
+            if x == "G" or x == "C":
+                counter +=1
+
+        percentatge = round(counter / len(adn),4)
+
+        return percentatge
+
+    def transcripcioARN(self):
         
